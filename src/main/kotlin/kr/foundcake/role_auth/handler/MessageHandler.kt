f448f8a2c4935d06f602eec150974b9fce1f4b78
found-cake <skymin468@gmail.com>
2024-03-14 14:04:39 +0900

    support sync

--- src/main/kotlin/kr/foundcake/role_auth/handler/MessageHandler.kt
+++ src/main/kotlin/kr/foundcake/role_auth/handler/MessageHandler.kt
@@ -1,5 +1,6 @@
 package kr.foundcake.role_auth.handler
 
+import dev.minn.jda.ktx.coroutines.await
 import dev.minn.jda.ktx.events.listener
 import kotlinx.coroutines.runBlocking
 import kr.foundcake.role_auth.database.DBManager
@@ -8,6 +9,7 @@ import kr.foundcake.role_auth.dto.UserDto
 import kr.foundcake.role_auth.extension.josa
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.Permission
+import net.dv8tion.jda.api.entities.Member
 import net.dv8tion.jda.api.entities.Role
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import java.io.BufferedReader
@@ -25,7 +27,7 @@ fun handleMessage(jda: JDA) {
 		val message = it.message
 		val content = message.contentRaw
 
-		if (message.attachments.count() != 1 || content != "<유저전체추가>") {
+		if (message.attachments.count() != 1 || (content != "<유저전체추가>" && content != "<유저전체추가>(동기화)")) {
 			return@listener
 		}
 		val attachment = message.attachments[0]
@@ -62,6 +64,14 @@ fun handleMessage(jda: JDA) {
 						)
 						if (result === SaveResult.FAILED) {
 							message.reply("이미 등록되어있습니다.($number 등록 실패)").queue()
+						} else if(content == "<유저전체추가>(동기화)"){
+							val members: List<Member> = it.guild.loadMembers().await()
+							for (m: Member in members) {
+								if (m.nickname == result.getUser()!!.nickName) {
+									it.guild.modifyMemberRoles(m, role).queue()
+									break
+								}
+							}
 						}
 					}
 				}
